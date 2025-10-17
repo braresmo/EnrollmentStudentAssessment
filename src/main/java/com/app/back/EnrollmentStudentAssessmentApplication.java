a0cc2c5a4296c741d8471ce7aeb6652bee9dacf9
braresmo <brandon1223@hotmail.com>
2025-10-17 17:11:40 +1100

    Sping java started and h2 database config

---
+++ src/main/java/com/app/back/EnrollmentStudentAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.app.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentStudentAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentStudentAssessmentApplication.class, args);
+	}
+
+}
