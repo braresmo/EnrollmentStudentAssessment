97031138ce1f137530e928913798da3f437bacbc
mert erdogan <merterdogan@merts-MacBook-Pro.local>
2025-10-18 00:03:29 +1100

    Permission model added

---
+++ src/main/java/com/app/back/model/Permission.java
@@ -0,0 +1,10 @@
+package com.app.back.model;
+
+
+
+public enum Permission {
+    COURSES_READ, COURSES_WRITE,
+    USERS_READ, USERS_WRITE,
+    GRADES_READ, GRADES_WRITE,
+    PAYMENTS_READ, PAYMENTS_REFUND
+}
